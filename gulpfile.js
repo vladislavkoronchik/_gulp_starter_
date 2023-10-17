f5c3c510d5e3c4467a3693de4f706adb6d4a22de
Vlad Koronchik <vladislavkoronchik@yandex.ru>
2023-10-17 15:09:08 +0300

    refactor: refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,56 @@
 import gulp from 'gulp';
-import {plugins} from './gulp/config/plugins.js';
+import { path } from './gulp/config/path.js';
+import { plugins } from './gulp/config/plugins.js';
 
+global.app = { 
+	isBuild: process.argv.includes('--build'),
+	isDev: !process.argv.includes('--build'),
+	path,
+	gulp,
+	plugins
+}
+
+import { copy } from './gulp/tasks/copy.js';
 import { html } from './gulp/tasks/html.js';
 import { scss } from './gulp/tasks/scss.js';
 import { scripts } from './gulp/tasks/scripts.js';
 import { images } from './gulp/tasks/images.js';
-import { videos } from './gulp/tasks/videos.js';
-import { fonts } from './gulp/tasks/fonts.js';
+import { sprite } from './gulp/tasks/sprite.js';
 import { clean } from './gulp/tasks/clean.js';
+import { copyWoff, fontsStyle, otfToTtf, ttfToWoff } from './gulp/tasks/fonts.js';
 
 const watching = () => {
 	plugins.sync.init({
-		server: './dist'
+		server: app.path.build.html
 	})
 
-	gulp.watch('src/scss/*.scss', scss)
-	gulp.watch('src/js/*.js', scripts)
-	gulp.watch('src/**/*.html', html).on('change', plugins.sync.reload)
-	gulp.watch('src/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}', gulp.series(images)).on('change', plugins.sync.reload)
-	gulp.watch('src/fonts/**/*.{ttf,otf,woff,woff2}', gulp.series(fonts)).on('change', plugins.sync.reload)
+	gulp.watch(app.path.watch.files, copy)
+	gulp.watch(app.path.watch.html, html)
+	gulp.watch(app.path.watch.scss, scss)
+	gulp.watch(app.path.watch.js, scripts)
+	gulp.watch(app.path.watch.images, images)
+
+	// Слежение за шрифтами и их автоматическая обработка.
+	// Раскомментировать, если ваш заказчик просит поиграться со шрифтами.
+	// В функции fontsStyle нужно убедиться, чтобы условие на 52-ой строке было закомментировано.
+	
+	gulp.watch(app.path.watch.fonts.otf, { queue: false }, otfToTtf)
+	gulp.watch(app.path.watch.fonts.ttf, { queue: false }, gulp.series(ttfToWoff, copyWoff))
+	gulp.watch(`${app.path.watch.fonts.buffer}*.woff2`, { queue: true, delay: 500 }, fontsStyle)
 }
 
-const build = gulp.series(clean, gulp.parallel(scss, html, scripts, images, videos, fonts));
-const serve = gulp.series(build, watching);
+const fonts = gulp.series(otfToTtf, ttfToWoff, copyWoff, fontsStyle);
+
+const mainTask = gulp.series(fonts, images, sprite, gulp.parallel(copy, scss, html, scripts));
+const build = gulp.series(clean, mainTask);
+const dev = gulp.series(build, watching);
 
 export {clean};
 export {images};
-export {videos};
+export {sprite};
+export {copyWoff};
+export {otfToTtf};
+export {ttfToWoff};
 export {fonts};
 export {build};
-export default serve;
+export default dev;
